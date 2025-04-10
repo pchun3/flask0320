30651611617f2bc4d6153243310c05bba88e13ce
pchun3 <s411087077@gmail.com>
2025-04-10 11:44:44 +0800

    v0

--- index.py
+++ index.py
@@ -1,7 +1,5 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
-cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
+from flask import Flask, render_template, request
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -10,10 +8,9 @@ def index():
 	homepage = "<h1>王珮淳Python網頁</h1>"
 	homepage += "<a href=/mis>MIS</a><br>"
 	homepage += "<a href=/today>顯示日期時間</a><br>"
-	homepage += "<a href=/welcome?nick=王珮淳&work=pu>傳送使用者暱稱</a><br>"
+	homepage += "<a href=/welcome?nick=王珮淳>傳送使用者暱稱</a><br>"
 	homepage += "<a href=/account>網頁表單傳值</a><br>"
 	homepage += "<a href=/about>王珮淳簡介網頁</a><br>"
-	homepage += "<br><a href=/read>讀取Firestore資料</a><br>"
 	return homepage	
 
 @app.route("/mis")
@@ -22,8 +19,7 @@ def mis():
 
 @app.route("/today")
 def today():
-	tz = timezone(timedelta(hours=+8))
-	now = datetime.now(tz)
+	now = datetime.now()
 	return render_template("today.html", datetime = str(now))
 
 @app.route("/about")
@@ -33,8 +29,7 @@ def about():
 @app.route("/welcome", methods=["GET"])
 def welcome():
 	user = request.values.get("nick")
-	w = request.values.get("work")
-	return render_template("welcome.html", name=user, work = w)
+	return render_template("welcome.html", name=user)
 
 @app.route("/account", methods=["GET", "POST"])
 def account():
@@ -46,16 +41,6 @@ def account():
 
 	else:
 		return render_template("account.html")
-		
-@app.route("/read")
-def read():
-    Result = ""
-    db = firestore.client()
-    collection_ref = db.collection("靜宜資管")    
-    docs = collection_ref.get()    
-    for doc in docs:         
-        Result += "文件內容：{}".format(doc.to_dict()) + "<br>"    
-    return Result
 
 if __name__ == "__main__":
 	app.run()
